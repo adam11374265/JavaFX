af8f82d7199a0b12064c4ddf57759ef91a692638
adam11374265 <47756151+adam11374265@users.noreply.github.com>
2020-08-18 23:11:41 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,45 +1,57 @@
-Package sample;
-import javafx.application.Application; 
-import javafx.scene.Group; 
-import javafx.scene.Scene; 
-import javafx.stage.Stage; 
-import javafx.scene.shape.Rectangle;
-import javafx.animation.AnimationTimer;
+package sample;
+
+import javafx.application.Application;
 import javafx.event.EventHandler;
+import javafx.scene.*;
+import javafx.scene.input.KeyEvent;
+import javafx.stage.Stage;
+import javafx.scene.shape.Rectangle;
+
+public class Main extends Application
+{
+
+    double W = 600, H = 400;
+    double x = 300, y = 200;
+
+    Rectangle rectangle = new Rectangle(150, 75, 150, 150);
 
 
+    @Override
+    public void start(Stage stage) throws Exception
+    {
 
-public class Main extends Application 
-{ 
-  private static final double W = 600, H = 400;
-  boolean goNorth,goSouth, goEast, goWest;
-  
-   public void start(Stage stage) throws Exception
-   { 
-      Rectangle rectangle = new Rectangle(150, 170, 150, 150);     
-      Group root = new Group(rectangle); 
-      Scene scene = new Scene(root, 600, 300);
-	scene.setOnKeyPressed(new EventHandler<KeyEvent>() 
+        Group root = new Group(rectangle);
+
+        rectangle.relocate(x, y);
+
+        Scene scene = new Scene(root, W, H);
+
+        scene.setOnKeyPressed(new EventHandler<KeyEvent>()
         {
-          @Override
-            public void handle(KeyEvent event) {
-              switch (event.getCode()) {
-                case UP:    goNorth = true; break;
-                case DOWN:  goSouth = true; break;
-                case LEFT:  goWest  = true; break;
-                case RIGHT: goEast  = true; break;
-             }
+            @Override
+            public void handle(KeyEvent event)
+            {
+                switch (event.getCode())
+                {
+                    case UP:    y -=1; rectangle.relocate(x, y); break;
+                    case DOWN:  y +=1; rectangle.relocate(x, y); break;
+                    case LEFT:  x -= 1; rectangle.relocate(x ,y); break;
+                    case RIGHT: x +=1; rectangle.relocate(x, y); break;
+                }
             }
         });
 
-        scene.setOnKeyReleased(new EventHandler<KeyEvent>() {
+        scene.setOnKeyReleased(new EventHandler<KeyEvent>()
+        {
             @Override
-            public void handle(KeyEvent event) {
-                switch (event.getCode()) {
-                    case UP:    goNorth = false; break;
-                    case DOWN:  goSouth = false; break;
-                    case LEFT:  goWest  = false; break;
-                    case RIGHT: goEast  = false; break;
+            public void handle(KeyEvent event)
+            {
+                switch (event.getCode())
+                {
+                    case UP:    y -=1; rectangle.relocate(x, y); break;
+                    case DOWN:  y +=1; rectangle.relocate(x, y); break;
+                    case LEFT:  x -= 1; rectangle.relocate(x ,y); break;
+                    case RIGHT: x +=1; rectangle.relocate(x, y); break;
                 }
             }
         });
@@ -47,49 +59,10 @@ public class Main extends Application
         stage.setScene(scene);
         stage.show();
 
-        AnimationTimer timer = new AnimationTimer() {
-            @Override
-            public void handle(long now) {
-                int dx = 0, dy = 0;
-
-                if (goNorth) dy -= 1;
-                if (goSouth) dy += 1;
-                if (goEast)  dx += 1;
-                if (goWest)  dx -= 1;
-                
-
-                moveRectangleBy(dx, dy);
-            }
-        };
-        timer.start();
     }
 
-    private void moveRectangleBy(int dx, int dy) {
-        if (dx == 0 && dy == 0) return;
-
-        final double cx = rectangle.getBoundsInLocal().getWidth()  / 2;
-        final double cy = rectangle.getBoundsInLocal().getHeight() / 2;
-
-        double x = cx + rectangle.getLayoutX() + dx;
-        double y = cy + rectangle.getLayoutY() + dy;
-
-        moveRectagleto(x, y);
-    }
-
-    private void moveRectagleto(double x, double y) {
-        final double cx = rectangle.getBoundsInLocal().getWidth()  / 2;
-        final double cy = rectangle.getBoundsInLocal().getHeight() / 2;
-
-        if (x - cx >= 0 &&
-            x + cx <= W &&
-            y - cy >= 0 &&
-            y + cy <= H) {
-            rectangle.relocate(x - cx, y - cy);
-        }
-    }
-
-    public static void main(String[] args) 
-    { 
-      launch(args); 
+    public static void main(String[] args)
+    {
+        launch(args);
     }
 }
